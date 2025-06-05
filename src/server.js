21d457feed6ebc72319b0f7d289d095f5d940443
Nataliia Tur <natalim@ukr.net>
2025-06-05 13:03:30 +0300

    Removed unnecessary nesting and added status

--- src/server.js
+++ src/server.js
@@ -21,18 +21,12 @@ export const setupServer = () => {
 
   app.use(express.json());
 
-  // app.use('/', (req, res) => {
-  //   res.json({
-  //     message: 'Hello!',
-  //   });
-  // });
-
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
     res.json({
       status: 200,
       message: 'Successfully found contacts!',
-      data: { contacts },
+      data: contacts,
     });
   });
 
@@ -42,19 +36,22 @@ export const setupServer = () => {
 
     if (!contact) {
       res.status(404).json({
+        status: 404,
         message: 'Contact not found',
       });
       return;
     }
 
     res.status(200).json({
+      status: 200,
       message: `Successfully found contact with id ${contactId}!`,
-      data: { contact },
+      data: contact,
     });
   });
 
   app.use((req, res, next) => {
     res.status(404).json({
+      status: 404,
       message: 'Not found',
     });
     next();
@@ -62,6 +59,7 @@ export const setupServer = () => {
 
   app.use((err, req, res, next) => {
     res.status(500).json({
+      status: 500,
       message: 'Something went wrong',
       error: err.message,
     });
