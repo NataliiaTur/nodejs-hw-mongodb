c55078d9aca40d8f2f8ee9f5f3a679654d6ffee3
Nataliia Tur <natalim@ukr.net>
2025-06-09 18:10:14 +0300

    homework 3

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -9,3 +9,24 @@ export const getContactById = async (contactId) => {
   const contact = await ContactsCollection.findById(contactId);
   return contact;
 };
+
+export const createContact = async (payload) => {
+  const contact = await ContactsCollection.create(payload);
+  return contact;
+};
+
+export const patchContact = async (contactId, payload, options) => {
+  const updatedContact = await ContactsCollection.findOneAndUpdate(
+    { _id: contactId },
+    { $set: payload },
+    { new: true },
+  );
+  return updatedContact;
+};
+
+export const deleteContact = async (contactId) => {
+  const deletedContact = await ContactsCollection.findOneAndDelete({
+    _id: contactId,
+  });
+  return deletedContact;
+};
